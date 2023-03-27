741945310be4f273b1a0db7799801bd62268ad09
Gitreposuser <vlad77solo@gmail.com>
2023-03-27 19:19:48 +0200

    add expression example command line version.

--- Algorithms/MathExpressions/src/Version2/ExpressionDemo.java
+++
@@ -1,9 +0,0 @@
-package Version2;
-
-public class ExpressionDemo {
-    public static void main(String[] args) {
-        ExpressionService expService = new ExpressionService();
-        String expression = expService.getExpressionFromUI();
-        System.out.println(expression);
-    }
-}
